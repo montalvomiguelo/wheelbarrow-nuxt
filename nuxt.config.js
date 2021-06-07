7104ddd56c7832157810f75da2900fb2ff5423e2
Miguel Montalvo <miguel.montalvo@barrelny.com>
2021-06-06 17:33:23 -0700

    :wrench: Add settings for slice machine

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,39 +6,56 @@ export default {
       lang: 'en',
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        charset: 'utf-8',
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: '',
+      },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/favicon.ico',
+      },
+    ],
   },
-
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
-
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
-
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
-
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
-    // https://go.nuxtjs.dev/stylelint
+    '@nuxtjs/eslint-module', // https://go.nuxtjs.dev/stylelint
     '@nuxtjs/stylelint-module',
     '@nuxtjs/prismic',
   ],
-
-  prismic: {
-    endpoint: 'https://montalvomiguelo.cdn.prismic.io/api/v2',
-    modern: true,
-  },
-
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
-
+  modules: [
+    [
+      '@nuxtjs/prismic',
+      {
+        endpoint: 'https://your-repo-name.cdn.prismic.io/api/v2',
+      },
+    ],
+    ['nuxt-sm'],
+  ],
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    transpile: ['vue-slicezone', 'nuxt-sm'],
+  },
+  storybook: {
+    stories: ['~/slices/**/*.stories.js', '~/slices/**/*.stories.js'],
+  },
+  ignore: ['**/*.stories.js', '**/*.stories.js'],
 }
